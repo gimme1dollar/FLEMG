061a87bd09c105e0e30259a5f5eccc1969cd6ce5
JYLee <gimme1dollar@postech.ac.kr>
2019-11-27 15:49:21 +0900

    Realtime Simulation ver2

--- P_realtime_simulation.py
+++ P_realtime_simulation.py
@@ -8,29 +8,37 @@ except ImportError:
 import matplotlib.pyplot as plt
 import matplotlib
 from pylab import *
+from drawnow import drawnow
 from random import seed
 from random import random
 from datetime import datetime
 
 # Utility
 def draw():
-  try:
-    print(testData[-1])
-    print(testLabel[-1])
-    print(prediction[0])
-    xNum = 10
-    xLen = 50
+  #try:
+    xList.append(iter)
+    xNum = len(xList)
+    xLen = 100
+
+    for i in range(Enco.flex_dim):
+      flex_data[i].append(testLabel[-1][i])
+      flex_pred[i].append(prediction[0][i])
+    for i in range(Enco.emg_dim):
+      emg_data[i].append(testData[-1][-1][i])
 
     plt.subplot(2, 1, 1)
     plt.axis([np.clip(xNum, 0, xNum-xLen), xNum-1, 0, 500])
-    plt.plot(xList, yList, '+')
-    plt.plot(xList, yList1, 'o')
+    for i in range( len(flex_data) ):
+      plt.plot(xList, flex_data[i], '+')
+    for i in range( len(emg_data) ):
+      plt.plot(xList, emg_data[i], 'o')
 
     plt.subplot(2, 1, 2)
     plt.axis([np.clip(xNum, 0, xNum-xLen), xNum-1, 0, 1000])
-    plt.plot(xList, yList2, 'o')
-  except:
-    print("draw excpetion")
+    for i in range( len(flex_pred) ):
+      plt.plot(xList, flex_pred[i], '+')
+  #except:
+  #  print("draw excpetion")
 
 # Instantiation
 start = time.time()
@@ -44,17 +52,28 @@ Prep = Processor.preprocessor(Enco)
 Network = Processor.network()
 
 plt.ion()
+fig=plt.figure(figsize=(8,8))
+flex_data = list()
+flex_pred = list()
+emg_data = list()
+xList = list()
+for i in range(Enco.flex_dim):
+  flex_data.append(list())
+  flex_pred.append(list())
+for i in range(Enco.emg_dim):
+  emg_data.append(list())
 
 FLEX.start()
 EMG.start()
 time.sleep(2)
 
+iter = 0
 with Network.graph.as_default():
   Network.construct_placeholders()
-  print("Model Constructed\n")
+  print("Model Constructed")
 
   Network.restore('./model/11252323(2)')
-  print("Model Restored\n")
+  print("Model Restored")
 
   try:
     while True:
@@ -76,8 +95,8 @@ with Network.graph.as_default():
         #index = np.arange(len(label))
         drawnow(draw)
         plt.pause(0.001)
-
+        iter += 1
   except KeyboardInterrupt:
-    print("keyboard interuupt")
+    print("keyboard interrupt")
     FLEX.exit()
     EMG.exit()
