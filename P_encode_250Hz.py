787fe66dc49e2ae691ea86ad93191df45d66db60
iju-yong <gimme1dollar@postech.ac.kr>
2020-01-04 16:03:40 +0900

    Update P_encode_250Hz.py
    100 EMG data every 0.4ms

--- P_encode_250Hz.py
+++ P_encode_250Hz.py
@@ -5,7 +5,7 @@ from datetime import datetime
 print("\n\nStart")
 
 # Open
-file_time = '202001022232'
+file_time = '202001041534'
 location = '250Hz_' + file_time + '.txt'
 file=open(location,'r')
 raw_data=file.read()
@@ -23,35 +23,40 @@ for d in range( len(raw_data) - 1 ):
   emg = data.split('_EMG_')[1]
 
   # Make Index
-  sec = int(float(index.split('\'')[1])//1) # second part
-  for t in range(250):
+  sec = float(index.split('\'')[1]) # second part
+  for t in range(100):
     tmp.append([sec + t*0.004])
 
   # EMG
   emg_set = emg.split(']')
   #print(f"{d} EMG samples : {len(emg_set)}")
+  
   emg_parts = []
   for e in range(len(emg_set)) :
-    if len(emg_set[e]) == 95: 
+    if len(emg_set[e]) > 80 and len(emg_set[e]) < 100: 
       emg_parts.append(emg_set[e].split(','))
 
-  for t in range( len(emg_parts) ): 
-    if t < 250: # pick 250 data
-      emg_parts[t][0] = emg_parts[t][0][1:]
-      for v in range( len(emg_parts[t]) ):
+  for t in range( len(emg_parts) ):
+    emg_parts[t][0] = emg_parts[t][0][1:]
+    for v in range( len(emg_parts[t]) ):
+      try:
         value = float(emg_parts[t][v])
         tmp[t].append(value)
-        
+      except:
+        #print(f"# {d} {t} has ValueError {emg_parts[t][v]}")
+        pass
+      
   # FLEX 
   flex_set = flex.split('\\n')
   #print(f"{d} FLEX samples : {len(flex_set)}")
+  
   flex_parts = []
   for f in range(len(flex_set)) :
     if len(flex_set[f]) == 19:
       flex_parts.append(flex_set[f].split(','))
 
-  for t in range( len(flex_parts) ): 
-    if t < 250: # pick 250 data
+  for t in range( len(flex_parts) ):
+    if t < 100:
       for v in range( len(flex_parts[t]) ):
         value = float(flex_parts[t][v])
         tmp[t].append(value)
