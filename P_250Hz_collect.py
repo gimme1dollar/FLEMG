47527f37d3d93a2270f54715bc31708f11811b11
iju-yong <gimme1dollar@postech.ac.kr>
2020-01-04 16:02:45 +0900

    Update P_250Hz_collect.py
    100 EMG data every 0.4ms

--- P_250Hz_collect.py
+++ P_250Hz_collect.py
@@ -20,10 +20,24 @@ class Sensor_FLEX(threading.Thread):
         self._exit = False
 
         self.storage = q
-        self.port = serial.Serial(p, b, timeout=1)  # (port name, baudrate, timeout)
+        self.port = serial.Serial(p, b, timeout=1) # (port name, baudrate, timeout)
+        
 
-        if self.port.is_open:
-            print('FLEX Serial port open')
+    def run(self):
+        while self.port.is_open:
+            data = self.port.read(1000)
+            self.storage.put(data)
+
+
+    def exit(self):
+        self._exit = True
+
+class Sensor_EMG(threading.Thread):
+    def __init__(self, q=queue.Queue(5000), p='COM5', b=115200):
+        threading.Thread.__init__(self)
+        self._exit = False
+        self.storage = q
+        self.port = serial.Serial(p, b, timeout=1)  # (port name, baudrate, timeout)
 
     def send(self, char):
         if self.port.is_open:
@@ -33,43 +47,12 @@ class Sensor_FLEX(threading.Thread):
             print("Serial closed")
 
     def run(self):
-        #start = time.time()
-        #count = 0
         while self.port.is_open:
-            data = self.port.read(250 * 25)
-            self.storage.put(data)
-
-            #if (time.time() - start > 4) and (time.time() - start < 5) :
-            #    count +=1
-            #elif (time.time() -start > 6) :
-            #    print(count)
-
-    def exit(self):
-        self.port.close()
-
-class Sensor_EMG(threading.Thread):
-    def __init__(self, q = queue.Queue(5000), p = 'COM5', b = 115200):
-        threading.Thread.__init__(self)
-        self._exit = False
-
-        self.storage = q
-        self.port = serial.Serial(p, b, timeout=1) # (port name, baudrate, timeout)
-        self.count = 0
-
-        if self.port.is_open:
-            print('EMG Serial port open')
-
-    def send(self, char):
-        print(f"Serial Write {char}")
-        self.port.write(bytearray(char, 'ascii'))
-
-    def run(self):
-        while self.port.is_open:
-            data = self.port.read(250 * 33)
+            data = self.port.read(5000)
             self.storage.put(data)
 
     def exit(self):
-        self.port.close()
+        self._exit = True
 
 
 # Instantiation
@@ -78,47 +61,43 @@ now = datetime.now()
 maxsize = 0xffffffff
 queue_list = [queue.Queue(maxsize), queue.Queue(maxsize)]
 
-FLEX = Sensor_FLEX(q=queue_list[0], p = 'COM6', b = 115200)
-EMG = Sensor_EMG(q=queue_list[1], p = 'COM4', b = 115200)
+FLEX = Sensor_FLEX(q=queue_list[0], p = 'COM4', b = 115200)
+EMG = Sensor_EMG(q=queue_list[1], p = 'COM2', b = 115200)
 
 FLEX.start()
-EMG.send("~5") # Sample Rate to 500Hz
-EMG.send('b') # Streaming Data
 EMG.start()
+start = time.time()
 dataSet = []
 try:
     while True:
         tmp = []
-        time.sleep(1)
+        queue_list[0].queue.clear()
+        queue_list[1].queue.clear()
+        time.sleep(0.4)
 
         index = time.time() - start
+        print("INDEX_"+str(index))
         tmp.append("INDEX_b\'" + str(index)+"\'")
 
-        f_q = queue_list[0].get()
-        tmp.append( "FLEX_" + str(f_q) )
+        f_q = list(queue_list[0].queue)
+        #print(f"f_q {f_q}")
+        tmp.append("FLEX_" + str(f_q) )
 
-        e_q = queue_list[1].get()
+        e_q = list(queue_list[1].queue)
+        #print(f"e_q {e_q}")
         tmp.append( "EMG_" + str(e_q) )
 
+        #print(f"tmp {tmp}")
         dataSet.append(tmp)
 
         #clear
-        queue_list[0].queue.clear()
-        queue_list[1].queue.clear()
 except KeyboardInterrupt:
     print("keyboard interuupt")
 
-    EMG.send('s')
     EMG.exit()
-    print("EMG exit")
     FLEX.exit()
-    print("FLEX exit")
-
-    print(np.asarray(dataSet).shape)
-    print(np.asarray(dataSet[2]))
 
     filename = str(now.year) + str(now.month) + str(now.day) + str(now.hour) + str(now.minute)
-    sav_loc = './data/250Hz_' + filename + '.txt'
+    sav_loc = './250Collector_' + filename + '.txt'
     np.savetxt(sav_loc, np.asarray(dataSet), fmt='%s', delimiter='___')
     print("TXT saved")
-
