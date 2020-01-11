a724858e482edb9bd4cf1032f9ac45fef70a6013
iju-yong <gimme1dollar@postech.ac.kr>
2020-01-11 14:50:48 +0900

    Feature (Average) Extractor
    update

--- modules/Processor.py
+++ modules/Processor.py
@@ -103,7 +103,9 @@ class preprocessor:
 		dataY = []
 		dataT = []
 		for i in range(len(self.raw) - self.encoder.seq_length + 1):
-			_x = self.raw[i:i+self.encoder.seq_length, self.encoder.index_dim:]
+			_x = self.raw[i:i+self.encoder.seq_length, self.encoder.index_dim:self.encoder.emg_dim]
+           label = raw[i:i + seq_length, self.encoder.data_dim-self.encoder.label_dim+1:]
+           _x = np.concatenate( (np.concatenate((_x, _x), axis=1), label), axis=1 )
 			_y = self.raw[i+self.encoder.seq_length-1, 1:] # Label이 emg+flex데이터가 되도록
 			_t = self.raw[i:i+self.encoder.seq_length, :self.encoder.index_dim]
 			dataX.append(_x)
@@ -115,6 +117,36 @@ class preprocessor:
 		self.label = np.array(dataY)
 
 		return self.index, self.data, self.label
+
+
+    def feature_average(self, average_unit1 = 10, average_unit2 = 100, data = None):
+		if data is not None:
+			self.raw = np.asarray(data)
+
+        res = []
+        for i in range(len(self.raw)):
+            if i > average_unit2:
+                tmp = [self.raw[i, :self.encoder.index_dim]]
+    
+                for ch in range(emg_length):
+                    tmp.append( self.raw[i, self.encoder.index_dim+ch] )
+                    
+                for ch in range(emg_length):
+                    avg1 = 0
+                    for r in range(average_unit1):
+                        avg1 += self.raw[i-r, self.encoder.index_dim+ch]
+                    tmp.append( avg1/average_unit1 )
+    
+                for ch in range(emg_length):
+                    avg2 = 0
+                    for r in range(average_unit2):
+                        avg2 += self.raw[i-r, self.encoder.index_dim+ch]
+                    tmp.append( avg2/average_unit2 )
+    
+                tmp = np.concatenate( (tmp, self.raw[i, 1+emg_length:]), axis=None)
+                
+                res.append(tmp.tolist())
+        return res
 
 class network_default:
 	def __init__(self, data_encoder = encoder()):
@@ -405,26 +437,121 @@ class network_feedback_whole:
 			if idx == 0:
 				for j in range(self.data_dim):
 					for l in range(self.data_encoder.seq_length):
-						testSet[idx, l, j] = default_
+						testSet[idx, l, self.data_encoder.emg_dim + j] = default_
 			elif idx < self.data_encoder.seq_length:
 				for j in range(self.data_dim):
 					for l in range(self.data_encoder.seq_length):
 						if l == self.data_encoder.seq_length:
-							testSet[idx, l, j] = test_predict[0][j]
+							testSet[idx, l, self.data_encoder.emg_dim + j] = test_predict[0][j]
 						elif l > idx:
-							testSet[idx, l, j] = testSet[idx-(self.data_encoder.seq_length-l), self.data_encoder.seq_length - 1, j]
+							testSet[idx, l, self.data_encoder.emg_dim + j] = testSet[idx-(self.data_encoder.seq_length-l), self.data_encoder.seq_length - 1, j]
 						else:
-							testSet[idx, l, j] = default_
+							testSet[idx, l, self.data_encoder.emg_dim + j] = default_
 			else:
 				for j in range(self.data_dim):
 					for l in range(self.data_encoder.seq_length):
 						if l == self.data_encoder.seq_length:
-							testSet[idx, l, j] = test_predict[0][j]
+							testSet[idx, l, self.data_encoder.emg_dim + j] = test_predict[0][j]
 						elif l > idx:
-							testSet[idx, l, j] = testSet[idx-(self.data_encoder.seq_length-l), self.data_encoder.seq_length - 1, j]
+							testSet[idx, l, self.data_encoder.emg_dim + j] = testSet[idx-(self.data_encoder.seq_length-l), self.data_encoder.seq_length - 1, j]
 			# Feed testData
 			test_predict = self.sess.run(self.Y_pred, feed_dict= {self.X: [testSet[idx]]})
 			prediction.append(test_predict[0])
+
+        # Calculate RMSE
+		if testLabel is not None:
+			rmse_val = self.sess.run(self.rmse, feed_dict={self.targets: testLabel, self.predictions: prediction})
+			print(f"RMSE: {rmse_val}\n")
+			return prediction, rmse_val
+		return prediction
+
+	def close(self):
+		tf.reset_default_graph()
+		self.sess.close()
+
+class network_feature_average:
+	def __init__(self, data_encoder = encoder()):
+		tf.set_random_seed(777)  # reproducibility
+		self.data_encoder = data_encoder
+		self.seq_length = data_encoder.seq_length
+		self.data_dim = data_encoder.flex_dim + (data_encoder.emg_dim * 3)
+		self.output_dim = data_encoder.flex_dim
+        
+		self.graph = tf.Graph()
+		self.sess = tf.Session()
+        
+		self.flag_kernel_opened = False
+		self.flag_placeholder = False
+
+	# build a LSTM network
+	def build_cell(self):
+		cell = tf.contrib.rnn.BasicLSTMCell(num_units = self.hidden_dim, state_is_tuple=True)
+		return cell
+
+	def construct_placeholders(self, learning_rate = 0.1, hidden_dim = 30, stak_dim = 2):
+		self.learning_rate = learning_rate
+		self.hidden_dim = hidden_dim
+		self.stack_dim = stak_dim
+
+		# Input Place holders
+		self.X = tf.placeholder(tf.float32, [None, self.seq_length, self.data_dim-self.output_dim])
+		self.Y = tf.placeholder(tf.float32, [None, self.output_dim])
+
+		# Build a LSTM network
+		multi_cells = tf.contrib.rnn.MultiRNNCell([self.build_cell() for _ in range(self.stack_dim)], state_is_tuple=True)
+		outputs, _states=tf.nn.dynamic_rnn(multi_cells, self.X, dtype=tf.float32)
+		self.Y_pred = tf.contrib.layers.fully_connected(outputs[:, -1], self.output_dim)
+
+		# Cost & Loss & Optimizer
+		self.loss = tf.reduce_sum(tf.square(self.Y_pred - self.Y))  # sum of the squares
+		self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
+		self.train = self.optimizer.minimize(self.loss)
+
+		# RMSE
+		self.targets = tf.placeholder(tf.float32, [None, self.output_dim])
+		self.predictions = tf.placeholder(tf.float32, [None, self.output_dim])
+		self.rmse = tf.sqrt(tf.reduce_mean(tf.square(self.targets - self.predictions)))
+        
+		self.flag_kernel_opened = True
+		self.flag_placeholder = True
+        
+	def train_network(self, training_data = [], training_label = [], iterations = 5000, location = 'model/_'):
+		self.sess = tf.Session(graph=self.graph)
+		init = tf.global_variables_initializer()
+		self.sess.run(init)
+
+		# Training step
+		for i in range(iterations):
+			_, step_loss = self.sess.run([self.train, self.loss], feed_dict={self.X: training_data, self.Y: training_label})
+			if(i) :
+				print(f"[step: {i}] loss: {step_loss}")
+		#train_predict = self.sess.run(self.Y_pred, feed_dict={self.X: training_data})
+
+		# Save Network
+		self.saver = tf.train.Saver()
+		location = location + "(" + str(self.stack_dim) + ")"
+		self.saver.save(self.sess, location+"/lstm.ckpt")
+
+	def restore(self, location='model/_'):
+		if (not self.flag_placeholder) :
+			self.construct_placeholders()
+		if (os.path.exists(location) == False):
+			print("Error : No such location")
+			return
+		self.sess = tf.Session(graph=self.graph)
+
+		self.saver = tf.train.Saver()
+		self.saver.restore(self.sess, tf.train.latest_checkpoint(location))
+
+	def infer(self, testSet = [], testLabel = None, default_ = 0.39):
+        prediction = []
+        for idx in range(len(testSet)) :
+            for j in range(self.data_encoder.label_dim):
+                for l in range(self.data_encoder.seq_length):
+                    testSet[idx, l, self.data_dim - self.output_dim + j] = testLabel[idx,j]
+            
+            test_predict = sess.run(Y_pred, feed_dict= {X: [testSet[idx]]})
+            prediction.append(test_predict[0])
 
         # Calculate RMSE
 		if testLabel is not None:
